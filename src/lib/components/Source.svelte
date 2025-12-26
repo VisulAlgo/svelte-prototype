be0c40b69762a7d5de9d542d492fb40075738d3c
Shedcrown <axdtora@gmail.com>
2025-12-26 10:56:24 +0300

    fix centering

--- src/lib/components/Source.svelte
+++ src/lib/components/Source.svelte
@@ -1,38 +1,39 @@
 <script lang="js">
-    import {page} from "$app/stores";
-    let {file} = $props();
-    const code = $page.data.codes?.[file]
+    import { page } from "$app/stores";
+    let { file } = $props();
+    const code = $page.data.codes?.[file];
 </script>
 
-
-<div class = "box">
+<div class="box">
     {@html code}
 </div>
 
 <style>
-.box {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-}
-:global(pre) {
-    width: 90%;
-    padding: 1em;
-    border-radius: 5px;
-    overflow: auto;
-}
-:global(code) {
-    counter-reset: step;
-    counter-increment: step 0;
-}
+    .box {
+        justify-content: center;
+        display: flex;
+        position: relative;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+    :global(pre) {
+        height: auto;
+        padding: 1em;
+        border-radius: 5px;
+        overflow: auto;
+    }
+    :global(code) {
+        counter-reset: step;
+        counter-increment: step 0;
+    }
 
-:global(code .line::before) {
-    content: counter(step);
-    counter-increment: step;
-    width: 1rem;
-    margin-right: 1.5rem;
-    display: inline-block;
-    text-align: right;
-    color: rgba(115,138,148,.4) 
-}
+    :global(code .line::before) {
+        content: counter(step);
+        counter-increment: step;
+        width: 1rem;
+        margin-right: 1.5rem;
+        display: inline-block;
+        text-align: right;
+        color: rgba(115, 138, 148, 0.4);
+    }
 </style>
--- src/lib/components/Stack.svelte
+++ src/lib/components/Stack.svelte
@@ -1,17 +1,23 @@
 <script lang="js">
-    import {Mermaid} from "@friendofsvelte/mermaid";
-    import {page} from "$app/stores";
-    let {file} = $props();
-    const raw = $page.data.diagrams?.[file]
+    import { Mermaid } from "@friendofsvelte/mermaid";
+    import { page } from "$app/stores";
+    let { file } = $props();
+    const raw = $page.data.diagrams?.[file];
     console.log(raw);
 </script>
 
 <div class="box">
-    <Mermaid string={raw}/>
+    <Mermaid string={raw} />
 </div>
 
 <style>
-.box {
-    width: 80%;
-}
+    .box {
+        width: 100%;
+        justify-content: center;
+        display: flex;
+        height: 50%;
+        position: relative;
+        top: 50%;
+        transform: translateY(-50%);
+    }
 </style>
